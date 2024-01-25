6b6c666392aa15d8effd7893c1746c349ed70200
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-01-25 13:17:08 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 import { promises as fs } from 'fs';
 import { data } from './data';
-import path from 'path';
-import { useRouter } from 'next/navigation';
+
+
 
 
 export default function Page() {
-    const router = useRouter();
-
-    router.push('/mercury');
     return (
         <div>
             <img src="assets/geology-earth.png" alt="" />
