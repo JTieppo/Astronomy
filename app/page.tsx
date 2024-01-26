bdc7e3d8f4f35e77cd088e54357cedd7bde57fa6
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-01-25 23:26:01 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client"
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 export default function Home(){
     return(
-        <div></div>
+        <div className="flex flex-col mt-32 w-screen mx-auto my-auto">
+            <div className="mx-auto my-auto items-center text-center">
+                <h1 className="mb-8 text-2xl">Ready to learn a little about the Milky Way?</h1>
+                <Link className="border rounded-lg p-3 w-20" href={"/mercury"}>Launch</Link>
+            </div>
+        </div>
     );
 }
