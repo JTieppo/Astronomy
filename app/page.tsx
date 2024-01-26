57c48f0f460c1a700f7a36966fdf92ba3d0b5242
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-01-26 00:00:46 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home(){
     return(
         <div className="flex flex-col">
-            <div className="m-20 items-center text-center h-10/12">
+            <div className="m-20 items-center text-center">
                 <h1 className="mb-8 text-2xl">Ready to learn a little about the Milky Way?</h1>
                 <Link className="border rounded-lg p-3 w-20" href={"/mercury"}>Launch</Link>
             </div>
