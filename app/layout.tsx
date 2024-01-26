bedf4a5f2e2e368206c90eb593968f794f5f0a9e
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-01-26 00:06:37 -0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <header className="hidden lg:flex flex-row justify-between mx-10 align-baseline">
+                <div className="hidden lg:flex flex-row justify-between mx-10 align-baseline">
                     <Link href="/mercury"><h2 className="text-4xl m-4">THE PLANETS</h2></Link>
                     <div className="flex flex-row items-center h-full jus">
                         <div className="m-10"></div>
@@ -52,13 +52,13 @@ export default function RootLayout({
                         <div className={`p-6 h-full border-t-2 border-hidden ${getPlanetClass(planetName)}`}><Link className="" id="navlink" href={"/uranus"}><h5 className={getPlanetClass(planetName)}>URANUS</h5></Link></div>
                         <div className={`p-6 h-full border-t-2 border-hidden ${getPlanetClass(planetName)}`}><Link className="" id="navlink" href={"/neptune"}><h5 className={getPlanetClass(planetName)}>NEPTUNE</h5></Link></div>
                     </div>
-                </header>
+                </div>
 
 
-                <header className="flex flex-row justify-between lg:hidden">
+                <div className="flex flex-row justify-between lg:hidden">
                     <Link className={`${valorNome} ml-3`} href="/mercury"><h2 className="text-4xl m-4">THE PLANETS</h2></Link>
                     <button className="ml-6" onClick={mostraMenu}><img className="mr-4" src="assets/icon-hamburger.svg" alt="" /></button>
-                </header>
+                </div>
                 <div className={`${valorMenu} lg:hidden flex-col`}>
                     <div className={`p-6 h-full border-t-2 border-hidden flex flex-row ${getPlanetClass(planetName)}`}><div className="p-3 rounded-full" style={{backgroundColor:"#419EBB"}}></div><Link className="ml-6"  id="navlink" onClick={mostraMenu} href={"/"}><h5 className={getPlanetClass(planetName)}>MERCURY</h5></Link></div>
                     <div className={`p-6 h-full border-t-2 border-hidden flex flex-row ${getPlanetClass(planetName)}`}><div className="p-3 rounded-full" style={{backgroundColor:"#EDA249"}}></div><Link className="ml-6"  id="navlink" onClick={mostraMenu} href={"/venus"}><h5 className={getPlanetClass(planetName)}>VENUS</h5></Link></div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home(){
     return(
-        <div className="flex flex-col">
+        <div className="flex flex-col h-screen">
             <div className="m-20 items-center text-center">
                 <h1 className="mb-8 text-2xl">Ready to learn a little about the Milky Way?</h1>
                 <Link className="border rounded-lg p-3 w-20" href={"/mercury"}>Launch</Link>
